47bb534ad3ff8d4ea9cd90422054eadcbb7d816a
shisan <shisan@gaoding.com>
2022-03-11 19:32:41 +0800

    chore: update deps

--- root/web-builder.config.ts
+++ root/web-builder.config.ts
@@ -2,10 +2,28 @@ import type { UserConfig, UserConfigExport } from '@growing-web/web-builder'
 import path from 'path'
 import fs from 'fs-extra'
 
-const siteConfig = fs.readJSONSync(path.resolve(__dirname, 'web-site.json'))
 
 export default function ({ mode }: UserConfigExport): UserConfig {
+
   const isDev = mode === 'development'
+  const siteConfig = fs.readJSONSync(path.resolve(__dirname, 'web-site.json'))
+
+  function transformFragment(string: string, data: any) {
+    return string.replace(
+      /<fragment\s+name="([^"]+)"[^>]?>([\w\W]*?)<\/fragment>/g,
+      (_, name, content) => {
+        const value = data[name]
+        const type = typeof value
+        if (type === 'string') {
+          return value
+        } else if (type === 'function') {
+          return value(content)
+        }
+        return ''
+      },
+    )
+  }
+
 
   function getFragment() {
     const manifestPath = path.resolve(
@@ -65,21 +83,6 @@ export default function ({ mode }: UserConfigExport): UserConfig {
   }
 
   return {
-    // 开发环境使用 web-dev-server
-    // web-dev-server 没有提供生产环境构建，所以使用了基于 rollup 的vite
-    bundlerType: 'webDevServer',
-
-    // 只有配置该选项，构建器会启动 web-site 模式
-    webSite: {
-      // 构建输出目录
-      outputDir: '../dist',
-      link: {
-        // link 源目录
-        src: '../',
-        // link 目标目录
-        target: './workspace',
-      },
-    },
     plugins: [
       !isDev
         ? {
@@ -92,6 +95,7 @@ export default function ({ mode }: UserConfigExport): UserConfig {
               }
               return null
             },
+
           }
         : {
             // FIXME 特殊处理，默认的插件不能前置处理html，所以使用 webDevServer专用插件处理
@@ -110,20 +114,4 @@ export default function ({ mode }: UserConfigExport): UserConfig {
           },
     ],
   }
-}
-
-function transformFragment(string: string, data: any) {
-  return string.replace(
-    /<fragment\s+name="([^"]+)"[^>]?>([\w\W]*?)<\/fragment>/g,
-    (_, name, content) => {
-      const value = data[name]
-      const type = typeof value
-      if (type === 'string') {
-        return value
-      } else if (type === 'function') {
-        return value(content)
-      }
-      return ''
-    },
-  )
 }
