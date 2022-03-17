71fa4df12a4a645f534880bf3a5396578cbcf98e
shisan <shisan@gaoding.com>
2022-03-17 10:17:59 +0800

    chore: release 0.0.1

--- apps/home-app/src/index.js
+++ apps/home-app/src/index.js
@@ -1,10 +1,10 @@
-import nav from 'nav';
+import nav from '@growing-web-examples/nav'
 export default () => ({
   mount({ container }) {
     container.innerHTML = `
       ${nav()}
       <p>Hello World</p>
-    `;
+    `
   },
   unmount({ container }) {
     container.innerHTML = ''
