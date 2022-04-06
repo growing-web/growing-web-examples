5b23128e00c23e8ca4a18550b1901c11d1fbcb1d
糖饼 <sugarpie.tang@gmail.com>
2022-04-06 11:30:45 +0800

    refactor: html 生成

--- root/scripts/generator-html.mjs
+++ root/scripts/generator-html.mjs
@@ -4,26 +4,24 @@ import { fileURLToPath } from 'url';
 import { transformFragment, JSONReader } from './utils.mjs';
 import { minify } from 'html-minifier-terser';
 
-const isDevelopment =  process.env.NODE_ENV === 'development';
+const nodeEnv = process.env.NODE_ENV || 'development';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const template = fs.readFileSync(`${__dirname}/template.html`, 'utf8');
 
 const html = transformFragment(template, {
   get importmap() {
     const importmap = JSONReader(`./importmap.json`);
-    const string = isDevelopment ? JSON.stringify(importmap, null, 2) : JSON.stringify(importmap);
+    const string = nodeEnv === 'development' ? JSON.stringify(importmap, null, 2) : JSON.stringify(importmap);
     return `
       <script type="importmap">${string}</script>
       <script>
-        (function() {
-          if (!HTMLScriptElement.supports || !HTMLScriptElement.supports('importmap')) {
-            document.head.appendChild(Object.assign(document.createElement('script'), {
-              src: 'https://es.dancf.com/npm:es-module-shims@0.12.8/dist/es-module-shims.min.js',
-              crossorigin: 'anonymous',
-              async: true
-            }));
-          }
-        })();
+        if (!HTMLScriptElement.supports || !HTMLScriptElement.supports('importmap')) {
+          document.head.appendChild(Object.assign(document.createElement('script'), {
+            src: 'https://es.dancf.com/npm:es-module-shims@0.12.8/dist/es-module-shims.min.js',
+            crossorigin: 'anonymous',
+            async: true
+          }));
+        }
       </script>
     `;
   },
@@ -47,6 +45,6 @@ const html = transformFragment(template, {
   }
 });
 
-const string = await (isDevelopment ? html : minify(html, { collapseWhitespace: true }));
+const string = await (nodeEnv === 'development' ? html : minify(html, { collapseWhitespace: true }));
 fs.writeFileSync('./index.html', string, 'utf8');
 console.log(`index.html created!`);
