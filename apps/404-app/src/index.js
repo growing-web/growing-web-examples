5e59f908bf1ef046e792ebb82426c13e708e5f9f
vben <anncwb@126.com>
2022-04-12 17:03:44 +0800

    fix: 修复示例运行错误

--- apps/404-app/src/index.js
+++ apps/404-app/src/index.js
@@ -1,6 +1,16 @@
 export default () => ({
   mount({ container }) {
-    container.innerHTML = '404'
+    container.innerHTML = `
+    <nav>
+      <a is="web-link" href="/">Home</a> |
+      <a is="web-link" href="/vue2">Vue2</a> |
+      <a is="web-link" href="/vue-router">Vue Router</a> |
+      <a is="web-link" href="/react">React</a> |
+      <a is="web-link" href="/404">404</a> |
+      <a is="web-link" href="https://google.com">Google</a>
+    </nav>
+    <div>404</div>
+    `
   },
   unmount({ container }) {
     container.innerHTML = ''
